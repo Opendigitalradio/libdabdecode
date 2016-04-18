b58c524c101d38b86d04b73c479b74441cece8a5
Felix Morgner <felix.morgner@gmail.com>
2016-04-18 13:54:24 +0200

    Update documentation

--- include/constants/convolution_encoder_attributes.h
+++ include/constants/convolution_encoder_attributes.h
@@ -4,24 +4,40 @@
 #include <array>
 #include <cstdint>
 
+/**
+ * @file
+ * @author Felix Morgner
+ * @copyright 3-Clause-BSD
+ *
+ * @brief This file contains constants derived from the DAB standard that apply to the convolution encode/decoder
+ *
+ * DAB uses convolution encoding to provide forward error correction. The basic properties of the encoder that are
+ * always the same are collected in this file. Note that the decoder can be derived from the encoders properties.
+ */
+
 namespace dabdecode
   {
 
-  /*
-   * @internal
-   *
-   * The length of the convolution encoder input as defined in ETSI EN 300 401 Section 11.1.1
-   */
+/**
+ * @internal
+ *
+ * @brief The length of the convolution encoder input as defined in ETSI EN 300 401 Section 11.1.1
+ *
+ * The encoder consumes dabdecode#kEncoderInputLength bits and produces dabdecode#kEncoderOutputLength parity
+ * bits. This is done in oder to achieve redundancy before transmission since this hardens the data against
+ * transmission errors.
+ */
   constexpr int kEncoderInputLength{1};
 
-  /*
-   * @internal
-   *
-   * The length of the convolution encoder output as defined in ETSI EN 300 401 Section 11.1.1
-   */
+/**
+ * @internal
+ *
+ * @brief The length of the convolution encoder output as defined in ETSI EN 300 401 Section 11.1.1
+ * @see dabdecode#kEncoderInputLength
+ */
   constexpr int kEncoderOutputLength{4};
 
-  /*
+  /**
    * @internal
    *
    * The polynomials of the convolution encoder as defined in ETSI EN 300 401 Section 11.1.1
@@ -30,13 +46,12 @@ namespace dabdecode
     0133, 0171, 0145, 0133
   }};
 
-  /*
+  /**
    * @internal
    *
    * The lookup table for expected symbols for the convolution decoder. Since we use softbits,
    * this table is "normalized" for softbits between around +-1.0f.
    */
-
   constexpr std::array<float, 64> kDecoderLookupTable {{
      0.7071067691,  0.7071067691,  0.7071067691,  0.7071067691,
      0.7071067691,  0.7071067691,  0.7071067691, -0.7071067691,
--- include/constants/puncturing_vectors.h
+++ include/constants/puncturing_vectors.h
@@ -4,20 +4,50 @@
 #include <array>
 #include <cstdint>
 
+/**
+ * @file
+ * @author Felix Morgner
+ * @copyright 3-Clause-BSD
+ *
+ * @brief This files contains the DAB standard puncturing vectors.
+ *
+ * As documented in convolution_encoder_attributes.h, DAB uses convolution coding for its
+ * data. Since convolution coding is associated with a rather large spacial overhead of 4
+ * in DAB, puncturing is used to 'reduce' the amount of redundancy. Since both the sender and
+ * receiver must use the same puncturing procedure, the vectors defined in this file are
+ * used. For more information on the encoding an puncturing, refer to ETSI EN 300 401 Section 11.
+ */
+
 namespace dabdecode
   {
 
-  /*
+  /**
    * @internal
    *
-   * Type alias for a regular puncturing vector.
+   * @brief Type alias for a regular puncturing vector.
+   *
+   * This type makes it possible to easily specify when a function takes or creates a
+   * puncturing vector. All puncturing vectors with the exception of the tail puncturing
+   * vector are exactly 32-bit long. We use a std::array<uint8_t, 32> to make the usage
+   * simpler. Additionally we do not need to synchronize access if we were to parallellize
+   * code that uses these vectors.
    */
   using regular_puncturing_vector_t = std::array<uint8_t, 32>;
 
-  /*
+  /**
    * @internal
    *
-   * The list of standard puncturing vectors.
+   * @brief The list of standard puncturing vectors.
+   *
+   * This list contains all standard puncturing vectors with the exception of the tail
+   * puncturing vector. All puncturing vectors in this list have the same length, which
+   * is guaranteed by using a std::array.
+   *
+   * The puncturing vectors are maintained in the exact same order as specified by the the DAB
+   * standard ETSI EN 300 401 Section 11.1.2.
+   *
+   * @note The numbering of the vectors is offset by -1 (thus starting at 0 not 1) simply for
+   * the sake of spacial efficiency.
    */
   constexpr std::array<regular_puncturing_vector_t, 24> kPuncturingVectors {{
     {{1,1,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0}},
@@ -46,17 +76,27 @@ namespace dabdecode
     {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}}
   }};
 
-  /*
+  /**
    * @internal
    *
-   * Type alias for the tail puncturing vector.
+   * @brief Type alias for the tail puncturing vector.
+   *
+   * This type alias makes it easy to declare functions that receive or return a puncturing
+   * vector for the tail bits of a DAB codeword. The convolution encoder used in DAB has a
+   * constraint length of 7, which results in 6 tail bits to clear the encoder shift registers.
+   * Since DAB uses a 1/4 code, 6 input bits result in 24 output bits thus resulting in a
+   * puncturing vector length of 24. For the rationale on the type choice, please refer to
+   * ::dabdecode::regular_puncturing_vector_t.
    */
   using tail_puncturing_vector_t = std::array<uint8_t, 24>;
 
-  /*
+  /**
    * @internal
    *
-   * The tail puncturing vector.
+   * @brief The tail puncturing vector.
+   *
+   * The DAB standard defines a special vector for the puncturing of the tail bits. This vector
+   * can be found in section 11 of ETSI EN 300 401.
    */
   constexpr tail_puncturing_vector_t kTailPuncturingVector {{
     1,1,0,0,1,1,0,0,1,1,0,0,1,1,0,0
