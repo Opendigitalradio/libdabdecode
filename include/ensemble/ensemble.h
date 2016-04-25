329fa2fb2bc670462010b987401a087f40238d78
Felix Morgner <felix.morgner@hsr.ch>
2016-04-25 10:01:05 +0200

    Add a bit of documentation

--- include/ensemble/ensemble.h
+++ include/ensemble/ensemble.h
@@ -15,8 +15,28 @@
 namespace dabdecode
   {
 
+  /**
+   * @brief Access information and data of a DAB ensemble
+   *
+   * This class provides acces to the information and data of a
+   * DAB ensemble. It gives access to services and subchannels.
+   */
   struct ensemble
     {
+    /**
+     * @brief Construct a @ref ensemble from a synchronization and a data stream.
+     *
+     * An ensemble is the most basic and interesting entity exposed by libdabdecode.
+     * It provides access to data services and audio services, also called programmes.
+     * The synchronization stream @p sync is expected to contain "zero" bytes unless
+     * the begin of a frame is reached. The data stream on the other hand must contain
+     * a single-precision 32-bit foating point stream of OFDM symbols. The transmission
+     * mode can be specified vi the @p mode argument, which default to DAB TM 1.
+     *
+     * @param sync The synchronization stream
+     * @param data The OFDM symbol data stream
+     * @param mode The transmission mode used for the ensemble
+     */
     ensemble(std::istream & sync, std::istream & data, transport_mode const mode = transport_mode::mode_1);
 
     std::string const & label() const;
